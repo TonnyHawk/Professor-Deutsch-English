ace54b24334157d4660bebec2fe64a9dd688f42b
TonnyHawk <anton.veremko@gmail.com>
2021-09-16 21:50:12 +0300

    pannel adding a student funtion added

--- src/pannel/webpack.config.js
+++ src/pannel/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
       // the target directory for all output files
       // must be an absolute path (use the Node.js path module)
       // the filename template for entry chunks
-      filename: 'bundle.[contenthash].js',
+      filename: 'bundle.js',
    },
    plugins: [
       new HtmlWebpackPlugin({
