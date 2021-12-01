ea24574a3743d16a31d07a8758bf778c9da640a6
TonnyHawk <anton.veremko@gmail.com>
2021-12-01 11:01:40 +0200

    updated functions

--- src/js/functions.js
+++ src/js/functions.js
@@ -1,15 +1,24 @@
 export function sortArr(arr, filter){ // sorting arr items by 'order' property and filter value
    let newArr = []
-   let length = arr.length;
-   // for(let i = 0; i < length; i++){
-   //    let foundIndex = arr.findIndex(elem=>elem.order[filter] === i);
-   //    newArr.push(arr[foundIndex])
-   // }
-   newArr = arr.sort((a, b)=>{
+   let ordered = []
+   let unordered = []
+
+   //splitting array on two types to avoid fails with undefined order property later
+   arr.forEach(elem=>{
+      if(Object.keys(elem).length > 0){
+         ordered.push(elem)
+      }else{
+         unordered.push(elem)
+      }
+   })
+
+   newArr = ordered.sort((a, b)=>{
       if(typeof a.order[filter] !== 'undefined'){
          return a.order[filter] - b.order[filter]
       }else return false;
    })
+
+   newArr = [...newArr, ...unordered]
 
    return newArr
 }
